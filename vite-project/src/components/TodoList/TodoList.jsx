5870ef5405df66011e1f31a00a2634c12f8fd635
Samo <safaryansamvel871@gmail.com>
2025-02-11 14:56:43 +0400

    Todo

--- vite-project/src/components/TodoList/TodoList.jsx
+++ vite-project/src/components/TodoList/TodoList.jsx
@@ -5,8 +5,6 @@ import AddBtn from "../AddBtn/AddBtn.jsx"
 function TodoList(){
     let [todos, setTodos] = useState([])
     let [text, setText] = useState('')
-    let [editText, setEditText] = useState('')
-    let [dblClick, setDblClick] = useState(false)
 
   let addTodo = () => {
     if(text.trim()){
@@ -42,8 +40,20 @@ function TodoList(){
     }))
   }
 
-  let edit = () => {
-    setDblClick(true)
+  let edit = (id) => {
+    let newTitle = prompt('Edit your text',
+      todos.find(todo => todo.id === id).title)
+    if(newTitle && newTitle.trim() !== ''){
+      setTodos(todos.map((todo) => {
+        if(todo.id === id){
+          return {
+            ...todo,
+            title: newTitle
+          }
+        }
+        return todo
+      }))
+    }
   }
 
   return(
@@ -57,7 +67,7 @@ function TodoList(){
               type="checkbox" 
               checked={todo.isDone} 
               onChange={() => change(todo.id)}/>
-            <span>{todo.title}</span>
+            <span onDoubleClick={() => edit(todo.id)}>{todo.title}</span>
             <button onClick={() => remove(todo.id)}>delete</button>
           </li>
         )
